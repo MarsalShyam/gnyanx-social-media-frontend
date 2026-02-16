e2b0239d2b389b046c9139d5af67319ff74522a6
MarsalShyam <shyam12marsal@gmail.com>
2026-02-16 17:15:04 +0530

    Project setup basics, css configuration and added assets

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Hii ram
+      <p className='text-3xl font-bold'>How are your?</p>
+    </div>
+  )
+}
+
+export default App
