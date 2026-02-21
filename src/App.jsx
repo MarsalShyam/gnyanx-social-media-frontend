e8c2fc676c562c6302a95102d795132e7b0ff5b6
MarsalShyam <shyam12marsal@gmail.com>
2026-02-21 13:25:06 +0530

    uploaded frontend till feed page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Feed from './pages/Feed'
+import Login from './pages/Login'
+import Messages from './pages/Messages'
+import ChatBox from './pages/ChatBox'
+import Discover from './pages/Discover'
+import Connection from './pages/Connection'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
+import { useUser } from '@clerk/clerk-react'
+// import {Layout} from './pages/Layout'
+import LayoutN from './pages/LayoutN'
+
 
 const App = () => {
+  const {user} =useUser()
   return (
-    <div>
-      Hii ram
-      <p className='text-3xl font-bold'>How are your?</p>
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={!user ? <Login/>: <LayoutN/>}>
+        <Route index element={<Feed/>}/>
+        <Route path='messages' element={<Messages/>}/>
+        <Route path='messages/:userId' element={<ChatBox/>}/>
+        <Route path='connections' element={<Connection/>}/>
+        <Route path='discover' element={<Discover/>}/>
+        <Route path='profile' element={<Profile/>}/>
+        <Route path='profile/:profileId' element={<Profile/>}/>
+        <Route path='create-post' element={<CreatePost/>}/>
+        <Route path='layoutn' element={<LayoutN/>}/>
+      </Route>
+    </Routes>
+    </>
   )
 }
 
